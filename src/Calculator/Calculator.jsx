84de3c915152c23b910280488beb97c7b50cdf9c
kyr(home) <webmake2050@gmail.com>
2025-06-24 14:17:15 +0900

    useState 추가

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { } from "react";
+import React, { useMemo, useState } from "react";
 
 const styles = {
   wrapper: {
@@ -25,16 +25,22 @@ const styles = {
 
 
 function Calculator() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const result = useMemo(() => {
+    return number1 + number2;
+  });
 
   return (
     <div style={{ margin: 10 }}>
       <h1>계산기 만들기</h1>
       <div style={styles.wrapper}>
-        <input type="number" style={styles.inputBox} />
-        <input type="number" style={styles.inputBox} />
+        <input type="number" style={styles.inputBox} value={number1} />
+        <input type="number" style={styles.inputBox} value={number2} />
         <button style={styles.btn}>첫번째 포커스</button>
       </div>
-      <div>결과 값 : </div>
+      <div>결과 값 : {result}</div>
     </div>
   );
 }
