826cbda50b03e087f817bfae887d6db9c53fe75c
kyr(home) <webmake2050@gmail.com>
2025-06-24 13:27:46 +0900

    html 넣기

---
+++ src/Calculator/Calculator.jsx
@@ -0,0 +1,14 @@
+
+function Calculator(){
+
+  return(
+    <div>
+      <h1>계산기 만들기</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>첫번째 포커스</button>
+      <div>결과 값</div>
+    </div>
+
+  );
+}
