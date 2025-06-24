1082b7431f95041eb56381e6716ed6617866654e
kyr(home) <webmake2050@gmail.com>
2025-06-24 14:22:21 +0900

    Callback 함수 추가

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 const styles = {
   wrapper: {
@@ -30,15 +30,19 @@ function Calculator() {
 
   const result = useMemo(() => {
     return number1 + number2;
-  });
+  }, [number1, number2]);
+
+  const handleChange1 = useCallback((e) => setNumber1(Number(e.target.value)), []);
+  const handleChange2 = useCallback((e) => setNumber2(Number(e.target.value)), []);
+
 
   return (
     <div style={{ margin: 10 }}>
       <h1>계산기 만들기</h1>
       <div style={styles.wrapper}>
-        <input type="number" style={styles.inputBox} value={number1} />
-        <input type="number" style={styles.inputBox} value={number2} />
-        <button style={styles.btn}>첫번째 포커스</button>
+        <input type="number" style={styles.inputBox} value={number1} onChange={handleChange1} />
+        <input type="number" style={styles.inputBox} value={number2} onChange={handleChange2} />
+        <button style={styles.btn} >첫번째 포커스</button>
       </div>
       <div>결과 값 : {result}</div>
     </div>
