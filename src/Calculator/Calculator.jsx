2bc5bf732be04c4c4ffe691176d1f6e34258edd6
kyr(home) <webmake2050@gmail.com>
2025-06-24 14:05:39 +0900

    리액트에 css 적용해 디자인 변경

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,16 +1,41 @@
-import React, {} from "react";
+import React, { } from "react";
 
-function Calculator(){
+const styles = {
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  inputBox: {
+    margin: 0,
+    marginRight: 5,
+    padding: 7,
+    border: "1px solid #ccc",
+  },
+  btn: {
+    margin: 0,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    border: "1px solid #ccc",
+  }
 
-  return(
-    <div>
+}
+
+
+function Calculator() {
+
+  return (
+    <div style={{ margin: 10 }}>
       <h1>계산기 만들기</h1>
-      <input type="text" />
-      <input type="text" />
-      <button>첫번째 포커스</button>
-      <div>결과 값</div>
+      <div style={styles.wrapper}>
+        <input type="number" style={styles.inputBox} />
+        <input type="number" style={styles.inputBox} />
+        <button style={styles.btn}>첫번째 포커스</button>
+      </div>
+      <div>결과 값 : </div>
     </div>
-
   );
 }
 
